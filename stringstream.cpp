873174011346da4e6bf3c5d0e25d25884f0bea83
Shariful Islam Naeem <118040047+Naeem739@users.noreply.github.com>
2024-10-05 01:28:32 +0600

    Add files via upload

---
+++ stringstream.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main(){
+	
+	
+	string s; 
+	getline(cin,s); 
+	
+	stringstream ss(s); 
+	string w; 
+	while(ss >> w){
+		 cout << w << endl; 
+	}
+}
